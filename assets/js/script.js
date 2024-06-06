7c25223bff0ee310944cf818b243b7a9408450bd
Hannah Smith <hannahpsmith94@gmail.com>
2024-06-06 16:44:20 -0600

    edited modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 
 
-// Todo: create a function to generate a unique task id
+//function to generate a unique task id
 function generateTaskId() {
     const timestamp = new Date().getTime();
     counter++;
